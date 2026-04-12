0ee05d685923dfca8110dc5886b25d975ca5a8c9
Amit Kumar <amitkumar4102004b@gmail.com>
2026-04-12 18:13:35 +0530

    added final code

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,3 +1,24 @@
+// import express from "express";
+// import multer from "multer";
+
+// const router = express.Router();
+
+// const storage = multer.diskStorage({
+//   destination: "uploads/",
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// router.post("/", upload.single("file"), (req, res) => {
+//   res.json({
+//     url: `http://localhost:5000/uploads/${req.file.filename}`,
+//   });
+// });
+
+// export default router;
 import express from "express";
 import multer from "multer";
 
@@ -14,7 +35,7 @@ const upload = multer({ storage });
 
 router.post("/", upload.single("file"), (req, res) => {
   res.json({
-    url: `http://localhost:5000/uploads/${req.file.filename}`,
+    url: `/uploads/${req.file.filename}`, // ✅ Relative path
   });
 });
 
